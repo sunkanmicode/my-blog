3c42bc64957eb7714036ca79bebe262f70558560
sunkanmicode <oguntimehins@gmail.com>
2021-07-09 13:04:13 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Topbar from "./topbar/Topbar";
+import Home from './page/home/Home'
+
+
+function App() {
+  return (
+    <>
+      <div>
+        <Topbar />
+        <Home />
+      </div>
+    </>
+    );
+  
+}
+
+export default App;
---
+++ src/header/Header.js
@@ -0,0 +1,14 @@
+import './header.css'
+
+export default function Header() {
+    return (
+        <div className='header'>
+            <div className='headerTitles'>
+                <span className='headerTitlesSm'>React & Node</span>
+                <span className='headerTitlesLg'>Blog</span>
+                <img className='headerImg' src='https://image.winudf.com/v2/image/Y29tLmxheWxhbGkuQmVhdXRpZnVsb2NlYW53YWxsX3NjcmVlbl8xX2VoOHV5N3Ry/screen-1.jpg?fakeurl=1&type=.jpg' alt='' />
+
+            </div>
+        </div>
+    )
+}
